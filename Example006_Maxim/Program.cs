24d542627c43d13700498a057fe220e803ee842e
Ogara <ya.rom4345@yandex.ru>
2022-09-15 14:03:13 +0300

    Находим максимальное из 3-х рандомных чисел

---
+++ Example006_Maxim/Program.cs
@@ -0,0 +1,12 @@
+﻿int numA = new Random().Next(1, 100);
+int numB = new Random().Next(1, 100);
+int numC = new Random().Next(1, 100);
+
+int max = numA;
+if (numB > max) max = numB;
+if (numC > max) max = numC;
+
+Console.WriteLine(numA);
+Console.WriteLine(numB);
+Console.WriteLine(numC);
+Console.Write(max);
