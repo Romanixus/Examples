6a177bbb3aff0f8553be0df86dd3568563c059f8
Ogara <ya.rom4345@yandex.ru>
2022-09-15 12:38:32 +0300

    add Masha

---
+++ Example005_Masha/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Name!");
+string username = Console.ReadLine();
+if (username == "Masha")
+{
+    Console.WriteLine("Ave, Maria!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
